c3e6485d6c63d2b84ac31419d91606eea5714e63
QueQAQ <74774932+QueQAQ@users.noreply.github.com>
2020-11-30 15:41:08 +0800

    113002.实现接口.App.java
    113002.实现接口.App.java

---
+++ 113002.实现接口.App.java
@@ -0,0 +1,14 @@
+package pac;
+
+public class App implements R {
+	public static void main(String[] args) {
+		System.out.print(R.boy_filePString);
+		System.out.print(R.girl_filePaht);
+		for (int row=0; row<array2d.length; row++){
+			for(int column=0;column<array2d[0].length;column++){
+				System.out.print(array2d[row][column]);
+			}
+		}
+	}
+
+}
