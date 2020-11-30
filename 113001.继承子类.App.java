7c60316cfa5e8eca371039dda9f525c1762ab734
QueQAQ <74774932+QueQAQ@users.noreply.github.com>
2020-11-30 15:40:05 +0800

    113001.继承子类.App.java
    113001.继承子类.App.java

---
+++ 113001.继承子类.App.java
@@ -0,0 +1,15 @@
+package pac;
+
+public class App extends R {
+	public static void main(String[] args) {
+		
+		System.out.print(girl_filePaht);
+		System.out.print(boy_filePString);
+		for (int row=0; row<array2d.length; row++){
+			for(int column=0;column<array2d[0].length;column++){
+				System.out.print(array2d[row][column]);
+			}
+		}
+	}
+
+}
