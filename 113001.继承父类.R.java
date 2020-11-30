256f79e5d41c6fee32e91f0124a1d191bd72e87c
QueQAQ <74774932+QueQAQ@users.noreply.github.com>
2020-11-30 15:39:03 +0800

    113001.继承父类.R.java
    113001.继承父类.R.java

---
+++ 113001.继承父类.R.java
@@ -0,0 +1,13 @@
+
+package pac;
+
+public class R {
+	
+	static String girl_filePaht="img/girl.gif";
+	static String boy_filePString="img/boy.gif";
+	static int [][] array2d={
+		{1,2},
+		{3,4},
+		{5,6}
+};
+}
