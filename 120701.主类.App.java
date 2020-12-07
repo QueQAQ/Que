a337bdacb3a9c4bcdad49ae7a6e074e4bbbc5cc3
QueQAQ <74774932+QueQAQ@users.noreply.github.com>
2020-12-07 15:37:17 +0800

    120701.主类.App.java
    120701.主类.App.java

---
+++ 120701.主类.App.java
@@ -0,0 +1,20 @@
+
+public class App {
+
+	
+	public static void main(String[] args) {
+		int array2d[ ][ ]={
+				{1,2,3,4,5,6},
+				{7,8,9,10,0,12},
+				{8,8,9,10,11,12},
+				{7,8,3,10,11,12},
+				{88,66,92,102,151,999}
+				};
+		new find().findZero(array2d);
+		new find().findZero1(array2d);
+		new find().findZero2(array2d);
+		new find().findZero3(array2d);
+		new find().findZero4(array2d);
+	}
+
+}
